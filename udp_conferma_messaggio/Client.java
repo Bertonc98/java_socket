f85a02a0c5767286fb4c307b1f9afd6464645c90
Bertonc98 <bertonc98@gmail.com>
2019-12-16 12:01:51 +0100

    Udp echo server

---
+++ udp_conferma_messaggio/Client.java
@@ -0,0 +1,47 @@
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+class Client{
+	public static void main(String[] arg){
+		//Se ha i giusti parametri
+		if(arg.length == 2){
+			DatagramSocket sClient;
+			DatagramPacket dp;
+			
+			//Lettura da tastiera
+			InputStreamReader keyboard = new InputStreamReader(System.in);
+			BufferedReader bf = new BufferedReader(keyboard);
+			
+			//Salva i parametri
+			String server_name = arg[0];
+			int server_port = Integer.parseInt(arg[1]);
+			String in;
+			byte[] buffer;
+			try{
+				do{
+					System.out.print("Message: ");
+					in = bf.readLine();
+					buffer = in.getBytes();
+					sClient = new DatagramSocket();
+					dp = new DatagramPacket(buffer, 0, buffer.length, InetAddress.getByName(server_name), server_port);
+					sClient.send(dp);
+					sClient.receive(dp);
+					System.out.println("Received: " + new String(buffer, 0, dp.getLength()));
+				}while(!in.equals("end"));
+				
+				System.out.println("Closing cliend");
+				sClient.close();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else
+			System.out.println("Improper argument");
+	}
+}
---
+++ udp_conferma_messaggio/Server.java
@@ -0,0 +1,45 @@
+import java.net.InetSocketAddress;
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Server{
+	public static void main(String[] arg){
+		InputStreamReader keyboard = new InputStreamReader(System.in);
+		BufferedReader bf = new BufferedReader(keyboard);
+		try{
+			//Crea la socket in autonomia e stampa indirizzo e porta scelti
+			DatagramSocket sServer = new DatagramSocket();
+			System.out.println("Server address: " + sServer.getLocalAddress() + "\nServer port: " + sServer.getLocalPort());
+			
+			//Crea il buffer in cui mettere il messaggio ricevuto
+			int dim = 100;
+			byte[] buffer = new byte[dim];
+			DatagramPacket dpIn = new DatagramPacket(buffer, 0, dim);
+			DatagramPacket echo;
+			String received = "init";
+			while(!received.equals("end")){
+				//Si mette in attesa di un messaggio e lo salva nel pacchetto dpIn, di conseguenza nel suo buffer
+				sServer.receive(dpIn);
+				
+				//Una volta ricevuto crea una stringa con ciò che è stato inserito nel buffer
+				received = new String(buffer, 0, dpIn.getLength());
+				System.out.println("Message received: " + received);
+				echo = new DatagramPacket(buffer, 0, dpIn.getLength(), 
+										  InetAddress.getByName(dpIn.getAddress().getHostName()), 
+										  dpIn.getPort());
+				sServer.send(echo);
+				System.out.println("Address: " + dpIn.getAddress().getHostName() + "\nPort: " + dpIn.getPort());
+				
+			}
+			System.out.println("Server shutdown...");
+			sServer.close();
+			}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
